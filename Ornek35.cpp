a33d4c86316553e1ff0348c75af7b4c944a0ea06
kasimkurt01 <101169075+kasimkurt01@users.noreply.github.com>
2025-11-05 09:24:27 +0300

    Add files via upload

---
+++ Ornek35.cpp
@@ -0,0 +1,55 @@
+// Matris Çarpýmý
+#include<iostream>
+#include<iomanip>
+#include "Ekran.h"
+    int a[3][3]={1,-1,3,1,2,1,-4,5,-1};
+	int b[3][3]={1,2,-1,3,-2,1,1,2,-1};
+    int c[3][3]={0};
+    
+void MatrisCarp(int a[3][3],int b[3][3], int c[3][3]){
+	
+	int i,j,k;
+    //Çarpma Ýþmeli baþlama
+    for(i=0;i<3;i++){
+    	for(j=0;j<3;j++){
+    		for(k=0;k<3;k++){
+    			c[i][j]=c[i][j]+a[i][k]*b[k][j];
+			}
+		}
+	}
+	//Çarpma iþlemi sonu
+	
+}
+void MatrisYaz(int a[3][3], int x1, int y1) {
+	int x=x1, y=y1;
+	
+	gotoXY(x,y); cout << char(218); 
+	gotoXY(x+11,y); cout << char(191); 
+	y++;
+	for(int i=0;i<3;i++) {
+		gotoXY(x,y) ; cout << char(179); 
+		for(int j=0;j<3;j++) 
+		 cout << setw(3)<<a[i][j] ;gotoXY(x+11,y) ; cout << char(179);
+		 y++;
+	}	
+	gotoXY(x,y); cout <<char(192); gotoXY(x+11,y); cout <<char(217);	
+	
+}  
+void Yazi(int x, int y , char a){
+	gotoXY(x+13,y+2); cout <<a;
+}
+int main(){
+//	setlocale(LC_ALL,"TURKISH");
+   	
+    MatrisCarp(a,b,c);
+	//Çarpma iþlemi sonu
+	
+	MatrisYaz(a,15,15);
+	Yazi(15,15,'*');
+	MatrisYaz(b,30,15);
+	Yazi(30,15,'=');
+	MatrisYaz(c,45,15);
+	
+	
+}
+     
